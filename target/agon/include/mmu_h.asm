f58dc62077f8ec6f4c62d072110f6d0f78105d82
Shawn Sijnstra <78007189+sijnstra@users.noreply.github.com>
2023-09-04 12:00:24 +1000

    Replacing over overzealous delete

---
+++ target/agon/include/mmu_h.asm
@@ -0,0 +1,12 @@
+; SPDX-FileCopyrightText: 2023 Zeal 8-bit Computer <contact@zeal8bit.com>
+;
+; SPDX-License-Identifier: Apache-2.0
+
+    IFNDEF MMU_H
+    DEFINE MMU_H
+
+        MACRO MMU_INIT _
+
+        ENDM
+
+    ENDIF
