e107b5a8a3d53c6f3609dfccde5a34c57fcde8be
Zeal8bit <contact@zeal8bit.com>
2025-09-01 23:59:02 +0800

    cmake: integrate zos_use function written by @zoul0813

--- cmake/zos_init.cmake
+++ cmake/zos_init.cmake
@@ -58,13 +58,15 @@ function(zos_add_asset target asset_file)
     target_sources("${target}" PRIVATE "${asm_file_path}")
 endfunction()
 
+# Add a list of assets to the current project: this function will generate one assembly file
+# per asset and add it to the current project source files
 function(zos_add_assets target)
     foreach(file IN LISTS ARGN)
         zos_add_asset(${target} ${file})
     endforeach()
 endfunction()
 
-
+# Convert the generated binary (IHX, ELF, ...) into a raw binary
 function(zos_add_outputs target)
     if(ZOS_TOOLCHAIN STREQUAL "sdcc")
         ihx_to_bin(${target})
@@ -72,3 +74,59 @@ function(zos_add_outputs target)
         elf_to_bin(${target})
     endif()
 endfunction()
+
+
+function(zos_use_search)
+    cmake_parse_arguments(PARSE_ARGV 0 ZOS_USE_SEARCH
+        "" "OUT" "")
+
+    if(NOT ZOS_USE_SEARCH_OUT)
+        message(FATAL_ERROR "zos_use_search: OUT <varname> mus tbe provided")
+    endif()
+
+    set(result "")
+
+    foreach(libname IN LISTS ZOS_USE_SEARCH_UNPARSED_ARGUMENTS)
+        # Extract the prefix (before first underscore) or whole name
+        string(REGEX MATCH "^[^_]+" prefix ${libname})
+
+        # Uppercase the prefix for env var lookup
+        string(TOUPPER "${prefix}" prefix_upper)
+
+        # Build possible env var names
+        set(env_candidates "${prefix_upper}_SDK_PATH" "${prefix_upper}_PATH")
+
+        # Find the first existing env var
+        unset(sdk_path)
+        foreach(env_var IN LISTS env_candidates)
+            if(DEFINED ENV{${env_var}})
+                set(sdk_path $ENV{${env_var}})
+                break()
+            endif()
+        endforeach()
+
+        if(NOT sdk_path)
+            message(FATAL_ERROR "Cannot find environment variable for package '${libname}' (tried ${env_candidates})")
+        else()
+            list(APPEND result "${sdk_path}/cmake/${prefix}_init.cmake")
+        endif()
+    endforeach()
+
+    # Export result to caller's scope
+    set(${ZOS_USE_SEARCH_OUT} "${result}" PARENT_SCOPE)
+endfunction()
+
+
+# Try to find a Zeal 8-bit OS SDK (or package/libraries) via a name.
+# This macro takes a string list as a parameter and will try to check any ENV variable as
+# UPPER(PARAM)_PATH or UPPER(PARAM)_SDK_PATH. If any exists, it will load the file:
+# FOUND_PATH/cmake/UPPER(PARAM).cmake
+#
+# Since the included cmake files may want to set global variables (package or toolcahin related),
+# this must be a macro.
+macro(zos_use)
+    zos_use_search(${ARGV} OUT sdk_paths)
+    foreach(path IN LISTS sdk_paths)
+        include(${path})
+    endforeach()
+endmacro()
