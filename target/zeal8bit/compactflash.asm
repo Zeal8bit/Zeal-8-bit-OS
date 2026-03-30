8148408a00942eb0dd024b0fbc05d3c78bb87154
Zeal8bit <contact@zeal8bit.com>
2026-03-30 19:32:10 +0800

    zeal8bit: implement write support for CompactFlash when using ZealFSv2

--- target/zeal8bit/compactflash.asm
+++ target/zeal8bit/compactflash.asm
@@ -54,7 +54,11 @@ cf_ready:
     jr nz, _cf_init_not_compatible
     ; Put disk letter in A, file system in E (rawtable) and driver structure in HL
     ld a, CF_DISK_LETTER
+  IF CONFIG_TARGET_COMPACTFLASH_RAWTABLE
     ld e, FS_RAWTABLE
+  ELIF CONFIG_TARGET_COMPACTFLASH_ZEALFS
+    ld e, FS_ZEALFS
+  ENDIF
     ld hl, _cf_driver
     call zos_disks_mount
     ; A has the status, return it if error
@@ -311,15 +315,344 @@ cf_not_implemented:
     ld a, ERR_NOT_IMPLEMENTED
     ret
 
-    ; API: Same as the read routine but for write. Not supported for CF (yet?).
+    ; Get the remaining sector size from a physical address
+    ; Parameters:
+    ;   HL - Bottom 16-bit address of a physical address
+    ; Returns:
+    ;   HL - Remaining size in the sector (1-511)
+    ; Alters:
+    ;   A, HL
+cf_remaining_sector_size:
+    ; Equivalent to HL = 512 - (HL & 511)
+    ld a, l
+    cpl
+    ld l, a
+    ld a, h
+    cpl
+    and 1
+    ld h, a
+    inc hl
+    ret
+
+
+    ; Calculate the minimum between HL and BC
+    ; Parameters:
+    ;   HL - Remaining size
+    ;   BC - Requested size
+    ; Returns:
+    ;   BC - Minimum between BC and HL
+    ; Alters:
+    ;   BC
+cf_min:
+    ld a, b
+    cp h
+    ret c
+    jr nz, _cf_min_hl_smaller
+    ld a, c
+    cp l
+    ret c
+_cf_min_hl_smaller:
+    ld b, h
+    ld c, l
+    ret
+
+
+    ; Increment a 32-bit byte address by one sector (512 bytes).
+    ; Parameters:
+    ;   HL - Lower 32-bit byte address
+    ;   [_cf_upper_addr] - Upper 16-bit of the 32-bit byte address
+    ; Returns:
+    ;   DEHL - Address + 512
+    ;   [_cf_upper_addr] - DE
+cf_advance_sector_addr:
+    ld a, h
+    add 2
+    ld h, a
+    ret nc
+    ld de, (_cf_upper_addr)
+    inc de
+    ld (_cf_upper_addr), de
+    ret
+
+
+    ; Wait until the CompactFlash requests data for a write operation.
+    ; Parameters:
+    ;   B - Timeout in loop count
+    ; Returns:
+    ;   A - 0 on success, 1 on error, 2 on timeout
+cf_wait_for_drq:
+    inc b
+_cf_wait_for_drq_loop:
+    dec b
+    jp z, _cf_timeout
+    in a, (CF_REG_STATUS)
+    bit STATUS_BUSY_BIT, a
+    jr nz, _cf_wait_for_drq_loop
+    bit STATUS_ERR_BIT, a
+    jr nz, _cf_wait_for_drq_error
+    bit STATUS_DRQ_BIT, a
+    jr z, _cf_wait_for_drq_loop
+    xor a
+    ret
+_cf_wait_for_drq_error:
+    ld a, 1
+    ret
+
+
+    ; Read a single 512-byte sector into the given buffer.
+    ; Parameters:
+    ;   HL - Lower 16-bit byte address within the CF
+    ;   [_cf_upper_addr] - Upper 16-bit byte address within the CF
+    ;   BC - Destination buffer
+    ; Returns:
+    ;   A - ERR_SUCCESS on success, ERR_FAILURE on error
+cf_read_sector_buffer:
+    push bc
+    ld a, 1
+    call cf_prepare_address_and_count
+    ld b, 255
+    call wait_for_ready
+    jr nz, _cf_read_sector_buffer_error
+    ld a, COMMAND_READ_SECTORS
+    out (CF_REG_COMMAND), a
+    ld b, 255
+    call wait_for_ready
+    jr nz, _cf_read_sector_buffer_error
+    pop hl
+    ld de, CF_SECTOR_SIZE
+_cf_read_sector_buffer_loop:
+    in a, (CF_REG_STATUS)
+    ASSERT(STATUS_BUSY_BIT == 7)
+    rlca
+    jr c, _cf_read_sector_buffer_loop
+    in a, (CF_REG_DATA)
+    ld (hl), a
+    inc hl
+    dec de
+    ld a, d
+    or e
+    jr nz, _cf_read_sector_buffer_loop
+    xor a
+    ret
+_cf_read_sector_buffer_error:
+    pop hl
+    ld a, ERR_FAILURE
+    ret
+
+
+    ; Write a single 512-byte sector from the given buffer.
+    ; Parameters:
+    ;   HL - Lower 16-bit byte address within the CF
+    ;   [_cf_upper_addr] - Upper 16-bit byte address within the CF
+    ;   BC - Source buffer
+    ; Returns:
+    ;   A - ERR_SUCCESS on success, ERR_FAILURE on error
+    ;   BC - Next buffer to write (source buffer + 512)
+cf_write_sector_buffer:
+    push bc
+    ld a, 1
+    call cf_prepare_address_and_count
+    ld b, 255
+    call wait_for_ready
+    jr nz, _cf_write_sector_buffer_error
+    ld a, COMMAND_WRITE_SECTORS
+    out (CF_REG_COMMAND), a
+    ld b, 255
+    call cf_wait_for_drq
+    jr nz, _cf_write_sector_buffer_error
+    pop hl
+    ld de, CF_SECTOR_SIZE
+_cf_write_sector_buffer_loop:
+    in a, (CF_REG_STATUS)
+    ASSERT(STATUS_BUSY_BIT == 7)
+    rlca
+    jr c, _cf_write_sector_buffer_loop
+    ld a, (hl)
+    out (CF_REG_DATA), a
+    inc hl
+    dec de
+    ld a, d
+    or e
+    jr nz, _cf_write_sector_buffer_loop
+    ld b, 255
+    call wait_for_ready
+    jr nz, _cf_write_sector_buffer_failure
+    ; Put HL (address of the next buffer) into BC
+    ld b, h
+    ld c, l
+    xor a
+    ret
+_cf_write_sector_buffer_error:
+    pop bc
+_cf_write_sector_buffer_failure:
+    ld a, ERR_FAILURE
+    ret
+
+
+    ; Write a block smaller than a sector at a sector-aligned address.
+    ; Parameters:
+    ;   HL - Lower 16-bit byte address within the CF, aligned on 512 bytes
+    ;   [_cf_upper_addr] - Upper 16-bit byte address within the CF
+    ;   BC - Size of the data to write (< 512)
+    ;   [_cf_buffer] - User buffer
+    ; Returns:
+    ;   A - ERR_SUCCESS on success, ERR_FAILURE on error
+cf_write_small_aligned_sector:
+    push hl
+    push bc
+    ld bc, _cf_sector_buffer
+    call cf_read_sector_buffer
+    pop bc
+    jr nz, _cf_write_small_aligned_sector_error
+    ld de, _cf_sector_buffer
+    ld hl, (_cf_buffer)
+    ldir
+    ld (_cf_buffer), hl
+    pop hl
+    ld bc, _cf_sector_buffer
+    jp cf_write_sector_buffer
+_cf_write_small_aligned_sector_error:
+    pop hl
+    ret
+
+
+    ; Write multiple complete sectors starting at the given byte address.
+    ; Parameters:
+    ;   HL - Lower 16-bit byte address within the CF, aligned on 512 bytes
+    ;   [_cf_upper_addr] - Upper 16-bit byte address within the CF
+    ;   A - Number of sectors to write
+    ;   BC - User buffer
+    ; Returns:
+    ;   A - ERR_SUCCESS on success, ERR_FAILURE on error
+    ;   BC - Next user buffer to write
+    ;   HL - Next byte address to write (lower 16-bit)
+cf_write_multiple_sectors:
+    push af
+    ; We must keep the lower 16-bit of the address in HL to write the next sector
+    push hl
+    ; This routine will also advance the BC buffer for us
+    call cf_write_sector_buffer
+    pop hl
+    jr nz, _cf_write_multiple_sectors_error
+    call cf_advance_sector_addr
+    pop af
+    dec a
+    jr nz, cf_write_multiple_sectors
+    xor a
+    ret
+_cf_write_multiple_sectors_error:
+    pop af
+    ld a, ERR_FAILURE
+    ret
+
+
+    ; API: Same as the read routine but for write.
 cf_write:
     or a
-    ld a, ERR_READ_ONLY
-    ; Directly return if the stack is cleaned
+    jp nz, cf_not_implemented
+    ld (_cf_buffer), de
+    ld (_cf_total_size), bc
+    ; Pop the upper 16-bit of the offset 
+    pop hl
+    ld (_cf_upper_addr), hl
+    pop hl
+    ld a, l
+    or a
+    jr nz, _cf_write_not_aligned
+    bit 0, h
+    jp z, _cf_write_aligned
+_cf_write_not_aligned:
+    ld (_cf_lower_addr), hl
+    push hl
+    call cf_remaining_sector_size
+    call cf_min
+    pop hl
+    ; Read the whole sector that is pointed by the address:
+    ; ([_cf_upper_addr] << 8) | HL
+    push bc
+    ld bc, _cf_sector_buffer
+    call cf_read_sector_buffer
+    pop bc
     ret nz
-    ; Clean the stack
-    pop hl
-    pop hl
+    ; The sector is ready in `_cf_sector_buffer`!
+    ; Keep the minimum size to write on the stack (BC). 
+    push bc
+    ld bc, _cf_sector_buffer
+    ; Calculate HL = _cf_sector_buffer + (offset & 511)
+    ld hl, (_cf_lower_addr)
+    ld a, h
+    and 1
+    ld h, a
+    add hl, bc
+    pop bc
+    ; Perform memcpy(_cf_buffer, HL, BC)
+    push bc
+    ex de, hl
+    ld hl, (_cf_buffer)
+    ldir
+    ; Get back the number of bytes written
+    pop bc
+    ; Resulting HL will be aligned on 512! It represents the next
+    ; buffers to write.
+    ld (_cf_buffer), hl
+    ; Check how many remaining bytes to write, store in BC
+    ; BC = total_size - min(cur_sector, total_size) 
+    ld hl, (_cf_total_size)
+    or a
+    sbc hl, bc
+    ; Store the remaining size on the stack
+    ld b, h
+    ld c, l
+    push bc
+    ; Write the sector we just modified
+    ld hl, (_cf_lower_addr)
+    ld bc, _cf_sector_buffer
+    call cf_write_sector_buffer
+    pop bc
+    ret nz
+    ; Check if we still have some bytes to write
+    ld a, b
+    or c
+    jr z, _cf_write_success
+    ; Advance the offset to the next sector
+    ld hl, (_cf_lower_addr)
+    call cf_advance_sector_addr
+    ; Fall-through
+_cf_write_aligned:
+    ; DEHL contains a sector-aligned offset/address
+    ; BC contain the number of bytes to write
+    push bc
+    ; Calculate the number of 512-byte sectors to write
+    ld a, b
+    srl a
+    ; If A is 0, no more full 512-byte sectors to write!
+    jr z, _cf_write_remaining
+    ; Write A sectors of 512-byte, starting at offset DEHL
+    ld bc, (_cf_buffer)
+    call cf_write_multiple_sectors
+    ; The routine cf_write_small_aligned_sector below will need the CF buffer
+    ld (_cf_buffer), bc
+    ; BC points to the next bytes to write
+    jr nz, _cf_write_failure_pop
+_cf_write_remaining:
+    ; Final sector to write, which has a size < 512 (partial write)
+    pop bc
+    ld a, b
+    and 1
+    ld b, a
+    or c
+    ; No more bytes? Success!
+    jr z, _cf_write_success
+    ; Write the final sector that is aligned on 512, but we have to write
+    ; less than 512 bytes.
+    call cf_write_small_aligned_sector
+    ret nz
+_cf_write_success:
+    xor a
+    ld bc, (_cf_total_size)
+    ret
+_cf_write_failure_pop:
+    pop bc
     ret
 
 
@@ -332,6 +665,11 @@ _cf_ignore_before: DEFS 2
     ; Total number of bytes to ignore after the read is performed (at most 511)
 _cf_ignore_after: DEFS 2
 _cf_upper_addr: DEFS 2
+_cf_lower_addr: DEFS 2
+
+_cf_buffer: DEFS 2
+_cf_total_size: DEFS 2
+_cf_sector_buffer: DEFS CF_SECTOR_SIZE
 
     SECTION KERNEL_DRV_VECTORS
 _cf_driver:
